0f78d1e9375f6399b26813d47e731d62b085e60a
Erkinbekov Bilim <erkinbekov298@gmail.com>
2025-11-14 16:14:01 +0600

    changed the squares

--- assign_two/ts/board.ts
+++ assign_two/ts/board.ts
@@ -3,9 +3,9 @@ let board: string = "";
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
     if ((i + j) % 2 === 0) {
-      board += "🟫";
+      board += "▨";
     } else {
-      board += "🟩";
+      board += "□";
     }
   }
   board += "\n";
